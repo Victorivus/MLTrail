163c2c425991d0bd2fc025abf2756874ff0be9de
Victorivus <vnacher@thinkdeep.ai>
2024-04-01 21:59:43 +0200

    Change from Flask+Jinja to Streamlit

--- front/app.py
+++ front/app.py
@@ -5,148 +5,25 @@ import random
 import string
 from datetime import timedelta
 import pandas as pd
-from flask import Flask, render_template, jsonify, request, send_file, session
-sys.path.append('../src/')
-from results.results import Results
-from scraper.scraper import Scraper
+import streamlit as st
 from dotenv import load_dotenv
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from src.scraper.scraper import Scraper
+from src.results.results import Results
 
 # Load variables from .env file
 load_dotenv()
 
-# Create the Flask app
-app = Flask(__name__)
-app.secret_key = os.environ.get('APP_SECRET')
-app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=7)  # Example: Session expires after 7 days
+# Function to store session-like data using Streamlit's caching mechanism
+@st.cache_data(hash_funcs={dict: lambda _: None})
+def get_session_data():
+    return {}
 
 # Initialize a Scraper instance
 scraper = Scraper()
 
-# Define the route for the main page
-@app.route('/')
-def index():
-    session['id'] = ''.join(random.choices(string.ascii_letters + string.digits, k=100))
-    # Get the list of events and years
-    events = dict(sorted(scraper.getEvents().items(), key=lambda item: item[1]))
-    # Remove years and strip
-    events = {key: ' '.join(word for word in value.split() if not word.isdigit() or len(word) != 4).strip() for key, value in events.items()}
-    events = {key: re.sub(r'^\d{4}|\d{4}$', '', value).strip() for key, value in events.items()}
-    # Remove French ordinals
-    events = {key: re.sub(r'(\d{1,2}(?:e|ème))', '', value).strip() for key, value in events.items()}
-    # Remove HTML tags
-    events = {key: re.sub(r'<[^<]+?>', '', value).strip() for key, value in events.items()}
-    # Sort alphabetically
-    events = dict(sorted(events.items(), key=lambda item: item[1]))
-
-    years = scraper.getEventsYears()
-    
-    # Pass the events to the template
-    return render_template('index.html', events=events, years=years)
-
-# Define an endpoint to get races
-@app.route('/get_races', methods=['POST'])
-def get_races():
-    # Get the selected event and year from the request data
-    event = request.json['event']
-    year = request.json['year']
-
-    # Set the event and year in the Scraper instance
-    scraper.setEvents([event])
-    scraper.setYears([year])
-
-    # Get the races for the selected event and year
-    races = scraper.getRaces()
-    
-    return jsonify(races)
-
-@app.route('/analysis', methods=['POST'])
-def generate_analysis():
-    if request.is_json:
-        event = request.json['event']
-        year = request.json['year']
-        race = request.json['race']
-    else:
-        event = request.form.get("events")
-        year = request.form.get("year")
-        race = request.form.get("race")
-
-    raw_results, control_points, rs, race_info = getRS(event, year, race)
-
-    folder_path = f'../data/plots/{event}'
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
-    file_path = os.path.join(folder_path,f'{event}_{race}_{year}.png')
-    rs.plotControlPoints(rs.getStats(),xrotate=True,inverty=True, savePath=file_path)
-
-    #TODO: Add button to toggle view between hours and time (apply or not rs.formatTimeOver24h)
-    data = {
-        'times': rs.times.applymap(rs.formatTimeOver24h),
-        'paces': rs.paces,
-        'plot_image_tag': f'<img src="/plot/{event}_{race}_{year}.png" alt="Matplotlib Plot">',
-        'event' : event,
-        'year' : year,
-        'race' : race
-    }
-
-    # Save for future request processing
-    session['event'] = event
-    session['year'] = year
-    session['race'] = race
-    session['race_info'] = race_info
-    
-    return render_template('analysis_template.html', data=data)
-
-@app.route('/plot/<path:image_name>', methods=['GET'])
-def get_plot(image_name):
-    folder_path = f'../data/plots/{scraper.events[0]}'
-    
-    # Construct the full path to the image file based on the provided image name
-    image_path = os.path.join(folder_path,image_name)
-    print(f'image_path: {image_path}')
-    # Use Flask's send_file function to send the image file in the response
-    return send_file(image_path, mimetype='image/png')
-
-@app.route('/plot/objective/<path:image_name>', methods=['GET'])
-def get_objective_plot(image_name):
-    folder_path = f'../data/plots/objective'
-    
-    # Construct the full path to the image file based on the provided image name
-    image_path = os.path.join(folder_path,image_name)
-    print(f'image_path: {image_path}')
-    # Use Flask's send_file function to send the image file in the response
-    return send_file(image_path, mimetype='image/png')
-
-@app.route('/loadObjective', methods=['POST'])
-def objective_form():
-    # Get the objective position from the form data
-    
-    event = session['event']
-    year = session['year']
-    race = session['race']
-    
-    _, _, rs, _ = getRS(event, year, race)
-
-    objective_time = request.form['objective']
-    objective_position = rs.getClosestTimeToObjective(objective_time)
-    rs.setObjective(objective_position)
-    obj = rs.getObjectivePaces()
-    mean_obj = rs.getObjectiveMeanPaces()
-    index = ['objective','mean(obj)']
-    paces = pd.concat([obj,mean_obj],ignore_index=True)
-    paces['index'] = index
-    paces.set_index('index',inplace=True)
-
-    folder_path = f'../data/plots/objective'
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
-    random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=100))
-    file_path = os.path.join(folder_path,f'{random_string}.png')
-    rs.plotControlPoints(paces, xrotate=True, inverty=True, savePath=file_path )
-
-    return jsonify({'img_path': f'/plot/objective/{random_string}.png'})
-
 def getRS(event, year, race):
-    
     scraper.setEvents([event])
     scraper.setYears([year])
     scraper.setRace(race)
@@ -166,6 +43,73 @@ def getRS(event, year, race):
 
     return raw_results, control_points, rs, race_info
 
+def main():
+    # Get the list of events and years
+    events = scraper.getEvents()
+    years = scraper.getEventsYears()
+
+    # Get the list of events and years
+    events = dict(sorted(scraper.getEvents().items(), key=lambda item: item[1]))
+    # Remove years and strip
+    events = {key: ' '.join(word for word in value.split() if not word.isdigit() or len(word) != 4).strip() for key, value in events.items()}
+    events = {key: re.sub(r'^\d{4}|\d{4}$', '', value).strip() for key, value in events.items()}
+    # Remove French ordinals
+    events = {key: re.sub(r'(\d{1,2}(?:e|ème))', '', value).strip() for key, value in events.items()}
+    # Remove HTML tags
+    events = {key: re.sub(r'<[^<]+?>', '', value).strip() for key, value in events.items()}
+    # Sort alphabetically
+    events = dict(sorted(events.items(), key=lambda item: item[1]))
+
+    event = st.selectbox('Select Event:', list(events.values()))
+    event = next(key for key, value in events.items() if value == event) # get key from value
+    year = st.selectbox('Select Year:', years[event])
+
+    # Get the races for the selected event and year
+    scraper.setEvents([event])
+    scraper.setYears([year])
+    races = scraper.getRaces()[event][year]
+
+    race = st.selectbox('Select Race:', list(races.values()))
+    race = next(key for key, value in races.items() if value == race) # get key from value
+
+    # Retrieve or initialize session-like data
+    session_data = get_session_data()
+
+    session_data['event'] = event
+    session_data['year'] = year
+    session_data['race'] = race
+    #session_data['race_info'] = race_info
+
+    # # Display session-like data
+    # st.write('Session Data:')
+    # st.write(session_data)
+    
+    st.title('Race Analysis')
+
+    # Display analysis results
+    if st.button('Generate Analysis'):
+        folder_path = f'../data/plots/{event}'
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+        file_path = os.path.join(folder_path, f'{event}_{race}_{year}.png')
+        raw_results, control_points, rs, race_info = getRS(event, year, race)
+        rs.plotControlPoints(rs.getStats(),xrotate=True,inverty=True, savePath=file_path)
+        data = {
+        'times': rs.times.map(rs.formatTimeOver24h),
+        'paces': rs.paces,
+        #'plot_image_tag': file_path,
+        'event' : event,
+        'year' : year,
+        'race' : race
+        }
+        # Display data
+        #TODO: Add button to toggle view between hours and time (apply or not rs.formatTimeOver24h)
+        st.write('Times:')
+        st.write(data['times'])
+
+        st.write('Paces:')
+        st.write(data['paces'])
+
+        st.image(file_path)
 if __name__ == '__main__':
-    # Run the Flask app
-    app.run(debug=True)
+    main()
