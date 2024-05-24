b11cbf403078788f778eca1f9cc14930c6f77852
Victorivus <vnacher@thinkdeep.ai>
2024-05-24 23:39:29 +0200

    Fix paths (still)

--- src/config/config.py
+++ src/config/config.py
@@ -1,3 +1,5 @@
 import os
-os.environ["PACKAGE_DIR_PATH"] = os.path.dirname(os.path.dirname(__file__))
+os.environ["PACKAGE_DIR_PATH"] = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 os.environ["DATA_DIR_PATH"] = os.path.join(os.environ["PACKAGE_DIR_PATH"], 'data')
+print(f"INFO: PACKAGE_DIR_PATH = {os.environ['PACKAGE_DIR_PATH']}")
+print(f"INFO: DATA_DIR_PATH = {os.environ['DATA_DIR_PATH']}")
