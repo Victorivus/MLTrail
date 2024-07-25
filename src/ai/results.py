eeb33b0e62cd8f41fd67934c8c932d701085c754
Victorivus <vnacher@thinkdeep.ai>
2024-07-25 16:02:46 +0200

    init Features DB Class

---
+++ src/ai/results.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import datetime as dt
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+
+class Features:
+    '''
+    Features class
+    '''
+    def __init__(self,) -> None:
+        return
