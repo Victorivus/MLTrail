1beb5c6a63aa3215fef62450c2619ac5823f0187
Victorivus <vnacher@thinkdeep.ai>
2024-08-04 19:21:43 +0200

    fix pipeline

--- src/config/config.py
+++ src/config/config.py
@@ -2,7 +2,11 @@ import os
 #from dotenv import load_dotenv
 
 if os.getenv("DATA_DIR_PATH") is None or os.getenv("PACKAGE_DIR_PATH") is None:
-    # load_dotenv()
+    load_dotenv(override=True)
     # print(f"INFO: PACKAGE_DIR_PATH = {os.environ['PACKAGE_DIR_PATH']}")
     # print(f"INFO: DATA_DIR_PATH = {os.environ['DATA_DIR_PATH']}")
-    print('INFO: Environemental variables not set PACKAGE_DIR_PATH, DATA_DIR_PATH')
+    print('INFO: Environemental variables were not set: PACKAGE_DIR_PATH, DATA_DIR_PATH')
+
+
+print(f"INFO: PACKAGE_DIR_PATH = {os.environ['PACKAGE_DIR_PATH']}")
+print(f"INFO: DATA_DIR_PATH = {os.environ['DATA_DIR_PATH']}")
