c76a5c60af039ba2b12d31f8552da1e3ecabf2b5
Victorivus <18736740+Victorivus@users.noreply.github.com>
2024-08-26 13:16:21 +0200

    Add AI trining to front

--- front/pages/2_my_results.py
+++ front/pages/2_my_results.py
@@ -6,10 +6,10 @@ import sqlite3
 import streamlit as st
 import pandas as pd
 from ai.features import Features
+from ai.xgboost import XGBoostRegressorModel
 
 DATA_DIR_PATH = os.environ["DATA_DIR_PATH"]
 DB_PATH = os.path.join(DATA_DIR_PATH, 'events.db')
-
 
 def fetch_distinct_names():
     '''
@@ -27,12 +27,10 @@ def fetch_results(surname, first_name=None):
     '''
     Function to execute query and fetch results
     '''
-    # Connect to SQLite database
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
 
     if first_name is None and ',' not in surname:
-        # Execute query to retrieve results
         query = """
             SELECT results.event_id, results.race_id, results.bib, events.name, events.year, results.race_id, results.position, results.cat_position,
                     results.full_cat_position, results.surname, results.name,
@@ -46,7 +44,6 @@ def fetch_results(surname, first_name=None):
     else:
         if ',' in surname:
             surname, first_name = [part.strip() for part in surname.split(",")]
-        # Execute query to retrieve results
         query = """
             SELECT results.event_id, results.race_id, results.bib, events.name, events.year, races.race_name, results.position, results.cat_position,
                     results.full_cat_position, results.surname, results.name,
@@ -63,83 +60,85 @@ def fetch_results(surname, first_name=None):
         cursor.execute(query, (surname.lower(), first_name.lower()))
 
     results = cursor.fetchall()
-
-    # Close database connection
     conn.close()
-
     return results
 
+def train_ai():
+    if 'metadata_features' in st.session_state:
+        metadata_features = st.session_state['metadata_features']
+        with st.spinner('Training the model... This might take a while.'):
+            feat = Features(metadata_features, DB_PATH)
+            data = feat.fetch_features_table().drop(columns=['id', 'race_id', 'event_id', 'bib'])
+            rgs = XGBoostRegressorModel(df=data, target_column='time')
+            rgs.train()  # Perform model training
+        st.session_state['model_params'] = rgs.model.get_params()
+        st.write("Model training completed.")
+        st.warning("Coming soon: Personalized time prediction in Race Lookup page.")
+        st.session_state.search_button_clicked = True
+    else:
+        st.write("Results data not found in session state.")
 
 def main():
     '''
-    main function for the Streamlit page
+    Main function for the Streamlit page
     '''
     st.set_page_config(layout="wide")
     st.title("Trail Race Results Lookup")
     st.markdown("*Scraped from LiveTrail up to 04/08/2024.*")
-    if 'result_button_clicked' not in st.session_state:
-        st.session_state.result_button_clicked = False
-    # Fetch distinct surnames and names from the database
-    # Too long, it lags.
-    # names = fetch_distinct_names()
+
+    # Initialize session state variables if they don't exist
+    if 'search_button_clicked' not in st.session_state:
+        st.session_state.search_button_clicked = False
+    if 'results_df' not in st.session_state:
+        st.session_state.results_df = None
+    if 'model_params' not in st.session_state:
+        st.session_state.model_params = None
+    if 'ai_trained_button_clicked' not in st.session_state:
+        st.session_state.ai_trained_button_clicked = False
+    if 'reset_button_clicked' not in st.session_state:
+        st.session_state.reset_button_clicked = False
 
     # User input for surname
     surname = st.text_input("Enter your `surname` or `surname, name`:")
 
     # Button to fetch results
-    if st.button("Search by free text"):
-        # Check if surname is provided
-        if surname:
-            # Fetch results from database
-            results = fetch_results(surname)
-
-            # Display results in table
-            if results:
-                results = pd.DataFrame(fetch_results(surname), columns=["event_id", "race_id", "bib", "Event", "Year", "Race",
-                                                                        "Position", "Sex Position",
-                                                                        "Category Position", "Surname",
-                                                                        "Name", "Sex Category",
-                                                                        "Full Category", "Time"])
-                
-                metadata = results[["event_id", "race_id", "bib"]]
-                metadata = list(metadata.itertuples(index=False, name=None))
-                results = results.drop(columns=["event_id", "race_id", "bib"])
-                st.session_state['metadata_features'] = metadata
-                st.write(results)
-                st.session_state.result_button_clicked = True
+    if st.button("Search"):
+        with st.spinner('Searching...'):    
+            if surname:
+                results = fetch_results(surname)
+                if results:
+                    results_df = pd.DataFrame(results, columns=["event_id", "race_id", "bib", "Event", "Year", "Race",
+                                                                "Position", "Sex Position", "Category Position", "Surname",
+                                                                "Name", "Sex Category", "Full Category", "Time"])
+                    st.session_state.results_df = results_df
+                    metadata = results_df[["event_id", "race_id", "bib"]]
+                    metadata = list(metadata.itertuples(index=False, name=None))
+                    st.session_state['metadata_features'] = metadata
+                    st.session_state.search_button_clicked = True
+                else:
+                    st.error("No results found for the provided surname.")
             else:
-                st.error("No results found for the provided surname.")
-        else:
-            st.warning("Please enter your surname.")
+                st.warning("Please enter your surname.")
 
-    if st.session_state.result_button_clicked:
-        if st.button("Convert to model input"):
-            if 'metadata_features' in st.session_state:
-                metadata_features = st.session_state['metadata_features']
-                feat = Features(metadata_features, DB_PATH)
-                st.write(feat.fetch_features_table())
-            else:
-                st.write("Results data not found in session state.")
-            st.warning("Not implemented yet. Feature copming soon...")
+    # Don't know why, but we need to press twice the Reset button...
+    # Check if the results are available in session state
+    if st.session_state.results_df is not None or st.session_state.reset_button_clicked:
+        st.write(st.session_state.results_df)
+        if st.session_state.model_params is None:
+            if st.button("Train AI model"):
+                train_ai()
+                st.session_state.ai_trained_button_clicked = True
+                st.session_state.reset_button_clicked = False
 
-    # name = st.selectbox("Select Name", options=names, index=0, format_func=lambda x: x.lower())
-    # # Button to fetch results
-    # if st.button("Get results"):
-    #     surname, first_name = name.split(", ")
-    #     results = fetch_results(surname, first_name)
-    #     # Display results in table
-    #     if results:
-    #         header = ["Event", "Year", "Race", "Position", "Sex Position",
-    #                   "Category Position", "Surname", "Name", "Sex Category",
-    #                   "Full Category", "Time"]
-    #         st.subheader("Results:")
-    #         st.table([header] + results)
-    #     else:
-    #         st.error("No results found for the provided name.")
-    # else:
-    #     st.warning("Please select a name.")
+    if st.session_state.model_params is not None and not st.session_state.reset_button_clicked:
+        if not st.session_state.ai_trained_button_clicked:
+            st.write("Model has already been trained. Parameters have been saved.")
+        if st.button("Reset Model"):
+            st.session_state.search_button_clicked = False
+            st.session_state.reset_button_clicked = True
+            st.session_state.model_params = None
+            st.session_state.ai_trained_button_clicked = False
+            st.write("Model has been reset. You can now train a new model.")
 
-
-# Run the app
 if __name__ == "__main__":
     main()
