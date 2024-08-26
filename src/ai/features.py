7a7f47b5957075427611239fee4614a9731793f6
Victorivus <18736740+Victorivus@users.noreply.github.com>
2024-08-26 17:34:13 +0200

    fix init + add random getter

--- src/ai/features.py
+++ src/ai/features.py
@@ -9,14 +9,14 @@ class Features:
     Features class
     '''
     # def __init__(self, event_id: int = None, race_id: str = None, bib: str = None, db: Database = None) -> None:
-    def __init__(self, results_metadata: list[tuple[int, str, str]], db: Database = None) -> None:
+    def __init__(self, results_metadata: list[tuple[int, str, str]], db_path: str = None) -> None:
         '''
         Args:
             results_metadata: Tuples containing (event_id, race_id, bib)
             db: App's database object
         '''
-        if db is not None:
-            self._db: Database = Database.create_database(db)
+        if db_path is not None:
+            self._db: Database = Database.create_database(db_path)
         else:
             self._db: Database = Database().create_database()
         # self._race_id: str = race_id
@@ -25,6 +25,9 @@ class Features:
         self._results_metadata = results_metadata
 
     def fetch_features_table(self) -> pd.DataFrame:
+        if self._results_metadata[-1][-1] == "":
+            self.fetch_anonymous_features_table()
+
         # Flatten list of tuples into a single list of parameters
         params = [item for sublist in self._results_metadata for item in sublist]
 
@@ -35,6 +38,27 @@ class Features:
             FROM features
             WHERE (event_id, race_id, bib) IN ({placeholders})
         '''
+        conn = sqlite3.connect(self._db.path)
+        try:
+            with conn:
+                df = pd.read_sql_query(query, conn, params=params)
+        finally:
+            conn.close()
+        return df
+    
+    def fetch_anonymous_features_table(self) -> pd.DataFrame:
+        # Flatten list of tuples into a single list of parameters
+        params = [item for sublist in self._results_metadata for item in sublist[:-1]]
+
+        # Construct the SQL query with the correct number of placeholders
+        placeholders = ','.join(['(?, ?)'] * len(self._results_metadata))
+        query = f'''
+                    SELECT DISTINCT race_id, event_id, dist_total, elevation_pos_total, elevation_neg_total,
+                        dist_segment, dist_cumul, elevation_pos_segment, elevation_pos_cumul, elevation_neg_segment,
+                        elevation_neg_cumul
+                    FROM features
+                    WHERE (event_id, race_id) IN ({placeholders})
+                '''
         conn = sqlite3.connect(self._db.path)
         try:
             with conn:
